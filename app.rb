4697cf33d6b0d33c9525514727e2ec9350d92abb
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-26 09:54:50 +0000

    bookmark list feature test but no bookmark /in app

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Bookmark Manager'
+    'Welcome to Bookmark Manager'
   end
 
   run! if app_file == $0
-end 
+end
--- spec/features/view_bookmark_spec.rb
+++ spec/features/view_bookmark_spec.rb
@@ -1,6 +1,15 @@
-feature 'Viewing bookmarks' do
+feature 'User can visit the home page' do
   scenario 'visiting the index page' do
     visit('/')
-    expect(page).to have_content "Bookmark Manager"
+    expect(page).to have_content "Welcome to Bookmark Manager"
   end
+
+feature "User can access list of bookmarks" do
+  scenario 'User views bookmark list' do
+    visit('/')
+    expect(page).to have_content "www.google.com"
+    expect(page).to have_content "wwww.amazon.co.uk"
+    expect(page).to have_content "wwww.asos.com"
+  end
+end
 end
